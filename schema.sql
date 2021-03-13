4c513b407342d3c884ecbf9a07208c6f2e29d84c
Mike Coletta <coletta.mike@gmail.com>
2021-03-12 16:39:52 -0800

    Added MySQL schema

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT ,
+  manager_id INT ,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL(10),
+department_id INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE department(
+id INT NOT NULL AUTO_INCREMENT,
+department_name VARCHAR(30),
+PRIMARY KEY (id)
+)
