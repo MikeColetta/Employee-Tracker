e631cbf6cb598ce884f3ccccfb6351b790d3244a
Mike Coletta <coletta.mike@gmail.com>
2021-03-17 10:02:04 -0700

    Updated README and formatted code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Employee-Tracker
+
+![GitHub](https://img.shields.io/github/license/MikeColetta/Employee-Tracker)
+
+## Description
+    
+I created a node.js CMS application that uses Inquirer and MySQL to add, update, and delete employees, roles, and departments for a company to track its employees.
+    
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Questions](#questions)
+5. [Credits](#credits)
+    
+## Installation
+    
+![Demonstration Video]()
+    
+## Usage
+
+![Screenshot1](./Images/Screenshot1.JPG)
+
+## License
+    
+MIT License
+    
+Copyright (c) 2021 Mike Coletta
+          
+## Questions?
+    
+[Github](https://github.com/MikeColetta)
+    
+For any questions, please send me an [email](mailto:coletta.mike@gmail.com).
--- seeds.sql
+++ seeds.sql
@@ -1,10 +1,10 @@
 USE employeetracker_db;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 16), ("Bob", "Doe", 2, 1), ("Jane", "Eng", 2, 1);
+VALUES ("John", "Doe", 1, NULL), ("Bob", "Doe", 2, 1), ("Jane", "Eng", 2, 1);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Manager", 50000, 1), ("Engineer", 75000, 2);
+VALUES ("Manager", 75000, 1), ("Lead Engineer", 100000, 2), ("Salesperson", 60000, 3), ("Engineer", 75000, 4), ("Lawyer", 75000, 5), ("Legal Team Lead", 90000, 6);
 
 INSERT INTO department (department_name)
 VALUES ("Sales"),("Engineering"),("Finance"),("Legal"), ("Human Resources");
@@ -15,8 +15,21 @@ SELECT * FROM role;
 
 SELECT * FROM employee;
 
+SELECT CONCAT("Emp ID: ", id, " - ", first_name, " ", last_name) AS Managers FROM employee WHERE role_id=1;
+
 SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.id, department.department_name FROM employee inner join role ON (employee.role_id = role.id) INNER JOIN department ON role.department_id = department.id;
 
 SELECT CONCAT(role.id, " - ", role.title) AS fullRole, CONCAT(department.id, " - ", department.department_name) AS fullDept FROM role INNER JOIN department ON (role.department_id = department.id); 
 
-DELETE FROM employee WHERE first_name='Mike';
+SELECT CONCAT(role.id, " - ", role.title) AS fullRole,
+     CONCAT(department.id, " - ", department.department_name) AS fullDept FROM role
+      INNER JOIN department ON (role.department_id = department.id);
+
+SELECT role.id, role.title, CONCAT(first_name, " ", last_name, " - ", role.title) AS fullName FROM employee FULL JOIN role ON (role_id = role.id);
+
+SELECT role.id, role.title, CONCAT(role.id, " ", role.title) AS fullRole, CONCAT(COALESCE(employee.first_name,""), " ", COALESCE(employee.last_name,"")) AS fullName FROM role LEFT JOIN employee ON role.id = employee.role_id;
+
+
+SELECT e1.id, e1.first_name, e1.last_name, e2.first_name, e2.last_name FROM employee e1 LEFT JOIN employee e2 ON (e1.manager_id = e2.id);
+
+SELECT role.id AS Employee_ID, title AS Title, salary AS Salary, department_id AS Department_ID, department_name AS Department FROM role INNER JOIN department ON (role.department_id = department.id);
