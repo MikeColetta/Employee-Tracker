03c0baba0db1032448100ad3001a1bd5d50b48c0
Mike Coletta <coletta.mike@gmail.com>
2021-03-18 19:27:43 -0700

    Fixed role bug by removing DEFAULT from the manager and role IDs on employee

--- db/schema.sql
+++ db/schema.sql
@@ -7,8 +7,8 @@ CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT DEFAULT NULL,
-  manager_id INT DEFAULT NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
   PRIMARY KEY (id)
 );
 
